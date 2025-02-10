e0ac56321d507a7292c6931b5f050faceccd59b8
Emily Prud'hommeaux <prudhome@bc.edu>
2025-02-10 10:39:25 -0500

    minor updates

--- nb.py
+++ nb.py
@@ -9,19 +9,20 @@ from textblob import TextBlob
 ########################
 
 ## hand-crafted list of stop words
-stops = {"'d", "'ll", "'m", "'ve", "'t", "'s", "'re", "a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "just", "let's", "me", "mightn't", "more", "most", "mustn't", "my", "myself", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "should've", "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "will", "with", "won't", "would", "wouldn", "wouldn't", "y'all", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", ",", ".", "!", "?", "'", '"', "I", "i"}
+stops = {"(", ")", "--", "*", ":", "-", "may", "though", ";", "thing", "things", "'d", "'ll", "'m", "'ve", "'t", "'s", "'re", "a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "just", "let's", "me", "mightn't", "more", "most", "mustn't", "my", "myself", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "should've", "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "will", "with", "won't", "would", "wouldn", "wouldn't", "y'all", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", ",", ".", "!", "?", "'", '"', "I", "i"}
 
 # these store the words used in positive and negative reviews
 # these will be populated in read_in_training_data()
 poswords = []
 negwords = []
 
-# these store the NB probability for each positive and negative word
+# these store the NB probability for each word in positive reviews ("positive word")
+# and each word in negative reviews ("negative word")
 # these will be populated in calculate_nb_probabilities()
 poswordprobs = {}
 negwordprobs = {}
 
-# these store the smoothed NB probability for each positive and negative word
+# these store the smoothed NB probability for each word 
 # these will be populated in calculate_smooth_nb_probabilities()
 smooth_poswordprobs = {}
 smooth_negwordprobs = {}
